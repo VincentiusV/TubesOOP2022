62c56b307963cad6806b5e36aed400a7c64e4b5f
mzakyyyy <mzakyy0@gmail.com>
2022-03-21 05:38:07 +0700

    update monster java

--- Monster.java
+++ Monster.java
@@ -3,4 +3,29 @@ import java.util.List;
 public class Monster {
     private String nama;
     private List<ElementType> elementTypeList;
+    private Stats baseStats;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public List<ElementType> getElementTypeList() {
+        return elementTypeList;
+    }
+
+    public void setElementTypeList(List<ElementType> elementTypeList) {
+        this.elementTypeList = elementTypeList;
+    }
+
+    public Stats getBaseStats() {
+        return baseStats;
+    }
+
+    public void setBaseStats(Stats baseStats) {
+        this.baseStats = baseStats;
+    }
 }
