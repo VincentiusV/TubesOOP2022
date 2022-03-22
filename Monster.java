e9dcca9727e9e47dd0d00fb3f913ce871ddb4401
mzakyyyy <mzakyy0@gmail.com>
2022-03-22 20:19:06 +0700

    coba main hehe

--- Monster.java
+++ Monster.java
@@ -5,6 +5,12 @@ public class Monster {
     private List<ElementType> elementTypeList;
     private Stats baseStats;
 
+    public Monster(String name, List<ElementType> elementTypeList, Stats baseStats) {
+        this.name = name;
+        this.elementTypeList = elementTypeList;
+        this.baseStats = baseStats;
+    }
+
     public String getName() {
         return name;
     }
@@ -27,5 +33,16 @@ public class Monster {
 
     public void setBaseStats(Stats baseStats) {
         this.baseStats = baseStats;
+    }
+
+    // Coba-coba aja
+    public void fight(Monster monster){
+        if (this.getBaseStats().getAttack() > monster.getBaseStats().getDefense()){
+            Stats newStats = new Stats(this.getBaseStats().getAttack()-monster.getBaseStats().getHP(),
+                                        monster.getBaseStats().getAttack(), monster.getBaseStats().getDefense(),
+                                        monster.getBaseStats().getSpecialAttack(), monster.getBaseStats().getSpecialDefense(),
+                                        monster.getBaseStats().getSpeed());
+            monster.setBaseStats(newStats);
+        }
     }
 }
