a836deaa9b2258dadb1782fe80f38f930dc09c77
VincentiusV <vincentiusverel@gmail.com>
2022-03-26 00:29:30 +0700

    Tambahin normal sama special move

---
+++ src/main/java/com/monstersaku/util/NormalMove.java
@@ -0,0 +1,19 @@
+package com.monstersaku.util;
+
+public class NormalMove extends Move {
+    protected int id;
+    protected String name;
+    protected ElementType elementType;
+    protected int accuracy;
+    protected int priority;
+    protected int ammunition;
+    
+    public NormalMove(String name, ElementType elementType, int accuracy, int priority, int ammunition){
+        super(name, elementType, accuracy, priority,ammunition);
+    }
+
+    public void useNormalMove (Monster monster1, Monster monster2){
+
+    }
+
+}
