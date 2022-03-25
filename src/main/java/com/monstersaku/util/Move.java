e73f755fce0f9de05470540b1eabcb55e418f458
VincentiusV <vincentiusverel@gmail.com>
2022-03-25 22:44:54 +0700

    update move setter getter

--- src/main/java/com/monstersaku/util/Move.java
+++ src/main/java/com/monstersaku/util/Move.java
@@ -8,12 +8,63 @@ public class Move{
     private int priority;
     private int ammunition;
 
-    public Move(int id, String name, ElementType elementType, int accuracy, int priority, int ammunition){
-        this.id = id;
+    //konstruktor
+    public Move(String name, ElementType elementType, int accuracy, int priority, int ammunition){
         this.name = name;
         this.elementType = elementType;
         this.accuracy = accuracy;
         this.priority = priority;
         this.ammunition = ammunition;
     }
+    
+
+    //getter & setter
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ElementType getElementType() {
+        return this.elementType;
+    }
+
+    public void setElementType(ElementType elementType) {
+        this.elementType = elementType;
+    }
+
+    public int getAccuracy() {
+        return this.accuracy;
+    }
+
+    public void setAccuracy(int accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getAmmunition() {
+        return this.ammunition;
+    }
+
+    public void setAmmunition(int ammunition) {
+        this.ammunition = ammunition;
+    }
+
 }
