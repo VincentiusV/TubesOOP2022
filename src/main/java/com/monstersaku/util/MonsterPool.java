f882afdb2b85f88a89ac65a1937bbaa20ac49b97
FlitzyBlue332 <18220031@std.stei.itb.ac.id>
2022-03-26 16:47:55 +0700

    Added MovePool and Adjustment on MonsterPool.java
    MovePool
    - Added MovePool
    - a pool of move
    MonsterPool
    - Added Comment

--- src/main/java/com/monstersaku/util/MonsterPool.java
+++ src/main/java/com/monstersaku/util/MonsterPool.java
@@ -3,13 +3,22 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
-
+/**
+ * MonsterPool is a pool of monster based on monsterpool.csv file.
+ * To get the list of monster, use method "getPool()".
+ * @author Muhammad Raihan Aulia 18220031
+ */
 public class MonsterPool {
     // idMove menjadi nomor di array
-    private String file_path = "../configs/monsterpool.csv";
+    private static String file_path = "../configs/monsterpool.csv";
     private List<Monster> pool = new LinkedList<Monster>();
+    private List<Move> movelist = new MovePool().getPool();
     
-    // Constructor
+    /**
+     * Creating a MonsterPool object.
+     * if and exception occured, outputs a warning.
+     * @return MonsterPool object.
+     */
     public MonsterPool(){
         try {
             CSVReader reader = new CSVReader(new File(MonsterPool.class.getResource(file_path).toURI()), ";");
@@ -21,25 +30,37 @@ public class MonsterPool {
 
             }
         } catch (Exception e) {
-          // do nothing
+          System.out.println("Maafkan saya, saya tidak dapat membaca \"monsterpool.csv\"");
         }
     }
-
+    /**
+     * Getter of Pool (list of monster) from MonsterPool class
+     * @return list of monster
+     */
     public List<Monster> getPool(){
         return pool;
     }
-
+    /**
+     * Turn an array of string to acceptable format to make a object of Monster
+     * @param input in form of array of string
+     * @return Monster object
+     */
     private Monster stringtoMonster(String[] input){
         String name = input[1];
-        List<ElementType> elementTypeList = stringtoListElementTypesList(input[2]);
+        List<ElementType> elementTypeList = stringtoListElementTypes(input[2]);
         Stats baseStats = stringtoStats(input[3]);
+        List<Move> movepool = moveCompare(input[4]);
         
 
-        Monster monster = new Monster(name, elementTypeList, baseStats);
+        Monster monster = new Monster(name, elementTypeList, baseStats, movepool);
         return monster;
     }
-
-    private List<ElementType> stringtoListElementTypesList(String input){
+    /**
+     * Turn string to acceptable format to make a list of element types
+     * @param input in form of string
+     * @return list object of element type
+     */
+    private List<ElementType> stringtoListElementTypes(String input){
         String arrString[] = input.split(",");
         List<ElementType> list = new LinkedList<ElementType>();
         for(String elementType : arrString){
@@ -58,6 +79,11 @@ public class MonsterPool {
         }
         return list;
     }
+    /**
+     * Turn string to acceptable format to make a Stats object.
+     * @param input in form of string
+     * @return Stats object.
+     */
     private Stats stringtoStats(String input){
         String arrString[] = input.split(",");
         double healthPoint = Double.parseDouble(arrString[0]);
@@ -72,19 +98,40 @@ public class MonsterPool {
         Stats bStats = new Stats(healthPoint, attack, defense, specialAttack, specialDefense, speed);
         return bStats;
     }
+    /**
+     * Comparing the moves in monsterpool.csv with MovePool
+     * @return List of move. List representing Monster's move
+     */
+    private List<Move> moveCompare(String input){
+        String[] arrString = input.split(",");
+        List<Move> monsterMove = new LinkedList<Move>();
 
+        for(String idString : arrString){
+            int id = Integer.parseInt(idString);
+            Move move = movelist.get(id-1);
+            monsterMove.add(move);
+        }
+        return monsterMove;
+    }
+
+    /**
+     * Print every monster in the movepool with their ElementType, Stats, and
+     * Move included.
+     */
     public void printMonsterPool(){
-        System.out.println("}=== POKEDEX ========================{");
+        System.out.println("}=== MONSTEDEX ========================{");
         for(Monster monster : pool){
             String name = monster.getName();
             List<ElementType> elementTypes = monster.getElementTypeList();
             Stats stat = monster.getBaseStats();
 
             // print Monster Name
-            System.out.println(String.format("%s ======================>", name));
+            System.out.print(String.format("%s ", name));
+            System.out.print("=".repeat(30-name.length()));
+            System.out.println(">");
 
             // print ElementType
-            System.out.println("Element Types: ");
+            System.out.print("Element Types : ");
             for(ElementType elementType : elementTypes){
                 if(elementType == ElementType.NORMAL){
                     System.out.println("NORMAL");
@@ -99,7 +146,15 @@ public class MonsterPool {
                     System.out.println("GRASS");
                 }
             }
+            // print Monster Stats
             stat.printStats();
+
+            // print Move set Monster
+            System.out.println("MOVE SET : ");
+            for(Move move : monster.getMoveList()){
+                System.out.println(move.getName());
+            }
+            System.out.println();
         }
     }
 }
---
+++ src/main/java/com/monstersaku/util/MovePool.java
@@ -0,0 +1,129 @@
+package com.monstersaku.util;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * MovePool is a pool of move based on movepool.csv file.
+ * To get the list of move, use method "getPool()".
+ * @author Muhammad Raihan Aulia 18220031
+ */
+public class MovePool {
+    // idMove menjadi nomor di array
+    private static String file_path = "../configs/movepool.csv";
+    private List<Move> pool = new LinkedList<Move>();
+    
+    public MovePool(){
+        try {
+            CSVReader reader = new CSVReader(new File(MovePool.class.getResource(file_path).toURI()), ";");
+            reader.setSkipHeader(true);
+            List<String[]> lines = reader.read();
+            for (String[] line : lines) {
+                String name = line[2];
+                ElementType elementType = stringtoElementType(line[3]);
+                int accuracy = Integer.parseInt(line[4]);
+                int priority = 1;
+                int ammunition = 10;
+                String target = line[7];
+    
+                // if the move type is normal move
+                if(line[1].equals("NORMAL")){
+                    NormalMove move = new NormalMove(name, elementType, accuracy, priority, ammunition);
+                    pool.add(move);
+                }
+                // if the move type is special move
+                else if(line[1].equals("SPECIAL")){
+                    SpecialMove move = new SpecialMove(name, elementType, accuracy, priority, ammunition);
+                    pool.add(move);
+                }
+                // If the move type is status move
+                else if(line[1].equals("STATUS")){
+                    String Condition = line[8];
+                    Stats changedStats = stringtoStats(line[9]);
+
+                    StatusMove move = new StatusMove(name, elementType, accuracy, priority, ammunition);
+                    pool.add(move);
+                }
+            }
+        } catch (Exception e) {
+          // do nothing
+          System.out.println("Maafkan saya tapi file movepool.csv tidak dapat dibaca.");
+        }
+    }
+
+    public List<Move> getPool(){
+        return pool;
+    }
+
+    private ElementType stringtoElementType(String input){
+        ElementType elementType = ElementType.NORMAL;
+        if(input.equals("NORMAL")){
+            elementType = ElementType.NORMAL;
+        }
+        else if(input.equals("FIRE")){
+            elementType = ElementType.FIRE;
+        }
+        else if(input.equals("WATER")){
+            elementType = ElementType.WATER;
+        }
+        else if(input.equals("GRASS")){
+            elementType = ElementType.GRASS;
+        }
+        return elementType;
+    }
+    private Stats stringtoStats(String input){
+        String arrString[] = input.split(",");
+        double healthPoint = Double.parseDouble(arrString[0]);
+        double attack = Double.parseDouble(arrString[1]);
+        double defense = Double.parseDouble(arrString[2]);
+        double specialAttack = Double.parseDouble(arrString[3]);
+        double specialDefense = Double.parseDouble(arrString[4]);
+        double speed = Double.parseDouble(arrString[5]);
+
+
+
+        Stats bStats = new Stats(healthPoint, attack, defense, specialAttack, specialDefense, speed);
+        return bStats;
+    }
+    
+    public void printMovePool(){
+        System.out.println("}=== MODEX ========================{");
+        for(Move move : pool){
+            System.out.println();
+            String name = move.getName();
+            ElementType elementType = move.getElementType();
+
+            // print Monster Name
+            System.out.print(String.format("%s ", name));
+            System.out.print("=".repeat(26-name.length()));
+            System.out.println("{");
+
+            // print ElementType
+            System.out.print("Element Types : ");
+            if(elementType == ElementType.NORMAL){
+                System.out.println("NORMAL");
+            }
+            else if(elementType == ElementType.FIRE){
+                System.out.println("FIRE");
+            }
+            else if(elementType == ElementType.WATER){
+                System.out.println("WATER");
+            }
+            else if(elementType == ElementType.GRASS){
+                System.out.println("GRASS");
+            }
+
+            // print classType
+            System.out.print("Move type     : ");
+            if(move instanceof NormalMove){
+                System.out.println("NORMAL MOVE");
+            }
+            else if(move instanceof SpecialMove){
+                System.out.println("SPECIAL MOVE");
+            }
+            else if(move instanceof StatusMove){
+                System.out.println("STATUS MOVE");
+            }
+        }
+    }
+}
